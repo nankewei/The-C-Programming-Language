ed31b320f11932c800899aeb3f57d8b54a757176
nankewei <35093286+nankewei@users.noreply.github.com>
2022-10-13 14:45:02 +0800

    编写一个程序，删除每个输入行末尾的空格和制表符，并删除完全是空格的行。

--- 第一章/1 - 18.c
+++ 第一章/1 - 18.c
@@ -3,7 +3,7 @@
 #define LONGLINE 80
 int getLine(char line[], int maxLine);
 // 编写一个程序，删除每个输入行末尾的空格和制表符，并删除完全是空格的行。
-//参考答案 把遇到换行符后的处理放到的一个新函数 remove,我在getLine函数实现了
+// 参考答案 把遇到换行符后的处理放到的一个新函数 remove,我在getLine函数实现了
 int main()
 {
     int len;               //当前行长度
@@ -33,7 +33,7 @@ int getLine(char line[], int maxLine)
     int c, i, j = 0;
     for (i = 0; (c = getchar()) != EOF && c != '\n'; i++)
     {
-        // 下标是0~999，用998存'\n' 999存'\0'
+        // 下标是0~999，如果有换行符 则用输入行长度的last but one 存'\n' last存'\0'，否则 直接用last 存'\0' 
         if (i < maxLine - 2)
         {
             line[j] = c;
@@ -60,7 +60,8 @@ int getLine(char line[], int maxLine)
         // j本来是保存换行符，最后一个空字符'\0'
         line[j] = c;
         ++j;
-        // 这里i++ 是为了在最后添加 空字符'\0',代表读取结束
+        // 这里j++ 是为了在最后添加 空字符'\0',代表读取结束
+        // i为长度
         i++;
     }
     line[j] = '\0';
