4062b486f02bf9e44d6b05f7bbd2ad778debfdf1
nankewei <35093286+nankewei@users.noreply.github.com>
2022-10-21 20:00:12 +0800

    重写折半查找

---
+++ 第三章/3 - 1.c
@@ -0,0 +1,21 @@
+/* binsearch: find x in v[0] <= v[1] <= ... <= v[n-1]    */
+int binsearch(int x, int v[], int n)
+{
+    int low, high, mid;
+ 
+    low = 0;
+    high = n - 1; 
+    mid = (low + high) / 2;
+    while(low <= high || x != v[mid])
+    {
+        if(x < v[mid])
+            high = mid - 1;
+        else
+            low = mid + 1;
+        mid = (low + high) / 2;
+    }
+    if(x == v[mid])
+        return mid;        /* found match    */
+    else
+        return -1;        /* no match        */
+}
