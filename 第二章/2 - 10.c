92169b9537628bbfc006e50c98f4af2a4b183d58
nankewei <35093286+nankewei@users.noreply.github.com>
2022-10-21 19:45:38 +0800

    重新编写将大写字母转换为小写字母的函数lower，并用条件表达式替代其中的if-else结构。

---
+++ 第二章/2 - 10.c
@@ -0,0 +1,6 @@
+// 重新编写将大写字母转换为小写字母的函数lower，并用条件表达式替代其中的if-else结构。
+/* lower: convert c to lower case (ASCII only)                  */
+int lower(int c)
+{
+    return c > 'A' && x <= 'Z' ? c + 'a' - 'A' : c;
+}
