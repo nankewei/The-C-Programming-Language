77a508f391bbb0c9b20b69a67bca3441cee438a5
nankewei <35093286+nankewei@users.noreply.github.com>
2022-10-30 16:47:50 +0800

    假定最多只压回一个字符。请相应修改getch与ungetch这两个函数。

---
+++ 第四章/4 - 8.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+char buf = 0;
+ 
+/* getch: get a (possibly pushed back) character             */
+int getch(void)
+{
+    int c;
+    if(buf != 0)
+        c = buf;
+    else
+        c = getchar();
+    buf = 0;
+    return c;
+}
+ 
+/* ungetch: push character back onto the input            */
+void ungetch(int c)
+{
+    if(buf != 0)
+        printf("ungetch: too many characters\n");
+    else
+        buf = c;
+}
