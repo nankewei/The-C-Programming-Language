80ab0673c22545175f6fcedb0462b6e77d13b0f8
nankewei <35093286+nankewei@users.noreply.github.com>
2022-10-30 16:50:10 +0800

    修改getop函数，使其不必使用ungetch函数。

---
+++ 第四章/4 - 11.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+#define NUMBER    '0'    /* signal that a number was found    */
+ 
+int getch(void);
+int getop(char s[])
+{
+    int i, c;
+    static int lastc = 0;
+    
+    if(lastc == 0)
+        c = getch();    
+    else
+    {
+        c = lastc;
+        lastc = 0;
+    }
+ 
+    while((s[0] = c == ' ' || c == '\t')
+        c = getch();
+    s[1] = '\0';
+    if(!isdigit(c) && c != '.')
+        return c;
+    i = 0;
+    if(isdigit(c))
+        while(isdigit(s[++i] = c = getch()));
+ 
+    if(c == '.')
+        while(isdigit(s[++i] = c = getch()));
+ 
+    s[i] = '\0';
+    if(c != EOF)
+        lastc = c;
+    return NUMBER;
+}
